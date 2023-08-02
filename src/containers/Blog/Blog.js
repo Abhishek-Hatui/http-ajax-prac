d86bdec3e6a2f79fc6433b2e1ead076509b659d8
hatuiabhishek <hatuiabhishek@gmail.com>
2023-08-02 15:10:24 +0530

    added navigation

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -50,7 +50,15 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>HOME</a></li>
+                            <li><a href='/new-post'>NEW POST</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
